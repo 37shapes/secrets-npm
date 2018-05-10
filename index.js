614bcec5e49489d74203bc1cf2dd65c2bade5fa2
Oleksandr Khomenko <alex@insiderx.com>
2018-05-10 18:06:37 -0500

    Added minimist for better cli experience Reorganized API and Cache, moved utility methods to Utils Merged Request and Login modules Added cli.js Normalized error messages

--- index.js
+++ index.js
@@ -1,37 +1,35 @@
 const path = require('path');
-const { SecretsLogin } = require('./lib/Login');
-const { SecretsRequest } = require('./lib/Request');
+
+const { SecretsAPI } = require('./lib/API');
 const { SecretsCache } = require('./lib/Cache');
 
 async function main() {
-  const login = new SecretsLogin();
+  const api = new SecretsAPI();
+
   let authToken;
   try {
-    authToken = await login.request('alex@khom', 'password');
+    authToken = await api.login('alex@khom', 'password');
     console.log(authToken);
   } catch (e) {
     console.log(e);
-    throw e;
   }
 
-  const secrets = new SecretsRequest(authToken);
-  const cache = new SecretsCache(path.join(__dirname, '.env'));
+  const cacheFilePath = path.join(__dirname, '.env');
+  const cache = new SecretsCache(cacheFilePath);
 
-  secrets.request()
-    .then((data) => {
-      cache.write(data);
-      console.log(data);
-    }, (err) => {
-      cache.read()
-        .then((data) => {
-          console.log(data);
-        }, (err) => {
-          if (err.code === 'ENOENT') {
-            console.error('Can not find cached version of the environment');
-          }
-          console.error(err);
-        });
-    });
+  let secrets;
+  try {
+    secrets = await api.secrets(authToken);
+    await cache.write(secrets);
+  } catch (e) {
+    console.log(e);
+    try {
+      secrets = await cache.read();
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  console.log(secrets);
 }
 
 main();
